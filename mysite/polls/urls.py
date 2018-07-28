2b20c7225580b7de46dcf69fc5fc930434d72f7d
Yang <InkYang@users.noreply.github.com>
2018-07-29 00:04:09 +0800

    sysModels

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     # ex: /polls/
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
     path('', views.index, name='index'),
     # ex: /polls/5/
     path('<int:question_id>/', views.detail, name='detail'),
